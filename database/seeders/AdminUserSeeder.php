ce0233878669da1992d546c35131c79acc303e05
Henry Lee <henryleeworld@gmail.com>
2024-03-12 00:13:22 +0800

    feat:更新最新資料

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class AdminUserSeeder extends Seeder
         User::create([
             'name' => 'Administrator',
             'email' => 'superadmin@booking.com',
-            'password' => 'SuperSecretPassword',
+            'password' => Hash::make('password'),
             'email_verified_at' => now(),
             'role_id' => 1, // Administrator
         ]);
