959ecb62931277db8167ceb42a9e054a09e08b07
Henry Lee <henryleeworld@gmail.com>
2025-08-16 00:18:31 +0800

    feat:更新最新資料

--- resources/views/vendor/scribe/themes/default/groups.blade.php
+++ resources/views/vendor/scribe/themes/default/groups.blade.php
@@ -1,11 +1,11 @@
 @foreach($groupedEndpoints as $group)
-    <h1 id="{!! Str::slug($group['name']) !!}">{!! $group['name'] !!}</h1>
+    <h1 id="{!! Str::slug($group['name'], language: app()->getLocale()) !!}">{!! $group['name'] !!}</h1>
 
     {!! Parsedown::instance()->text($group['description']) !!}
 
     @foreach($group['subgroups'] as $subgroupName => $subgroup)
         @if($subgroupName !== "")
-            <h2 id="{!! Str::slug($group['name']) !!}-{!! Str::slug($subgroupName) !!}">{{ $subgroupName }}</h2>
+            <h2 id="{!! Str::slug($group['name'], language: app()->getLocale()) !!}-{!! Str::slug($subgroupName, language: app()->getLocale()) !!}">{{ $subgroupName }}</h2>
             @php($subgroupDescription = collect($subgroup)->first(fn ($e) => $e->metadata->subgroupDescription)?->metadata?->subgroupDescription)
             @if($subgroupDescription)
                 <p>
